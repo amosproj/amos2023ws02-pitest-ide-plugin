68ba744fd79950ad8319e4daca6cc04cc4ef32da
Tim Herzig <tim.herzig@hotmail.com>
2023-11-12 13:31:52 +0100

    Action finding filename
    Co-authored-by: Brianne Oberson <brianne.oberson@gmail.com>
    Signed-off-by: Tim Herzig <tim.herzig@hotmail.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/RunConfigurationAction.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/RunConfigurationAction.kt
@@ -10,21 +10,26 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.ui.Messages
-import com.intellij.pom.Navigatable
-import org.apache.tools.ant.Project
+import com.intellij.psi.PsiDocumentManager
 
 
-class RunConfiguration : AnAction() {
+class RunConfigurationAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         println("RunConfigure Action Performed")
 
         var project: com.intellij.openapi.project.Project? = e.project
         var message: StringBuilder = StringBuilder(e.presentation.text + " Selected!")
-        var selElement: Editor? = e.getData(CommonDataKeys.EDITOR_EVEN_IF_INACTIVE) //NAVIGATABLE)
-        if (selElement != null) {
-            var selDocument: Document? = selElement.document
-            
-            message.append("\nSelected Element: ").append(selElement)
+        var editor: Editor? = e.getData(CommonDataKeys.EDITOR_EVEN_IF_INACTIVE) //NAVIGATABLE)
+        var name: String = ""
+        if (editor != null) {
+            var document: Document? = editor.document
+            if (document != null) {
+                var psi = PsiDocumentManager.getInstance(project!!).getPsiFile(document)
+                if (psi != null) {
+                    message.append("\nSelected Element: ").append(psi.name)
+                    name = psi.name
+                }
+            }
         }
         var title: String = e.presentation.description
         Messages.showMessageDialog(
@@ -33,6 +38,7 @@ class RunConfiguration : AnAction() {
             title,
             Messages.getInformationIcon()
         )
+        val runConfig = RunConfiguration(name)
     }
 
     override fun update(e: AnActionEvent) {
@@ -46,3 +52,5 @@ class RunConfiguration : AnAction() {
     }
 }
 
+class RunConfiguration(val fileName: String) {
+}
