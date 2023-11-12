205e5e009a78ba978f81d0bd3c0dd280e66e95d2
Tim Herzig <tim.herzig@hotmail.com>
2023-11-12 12:51:42 +0100

    toolbar action implemented as proof of concept

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/RunConfiguration.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/RunConfiguration.kt
@@ -3,5 +3,46 @@
 
 package com.amos.pitmutationmate.pitmutationmate
 
-class RunConfiguration {
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.ui.Messages
+import com.intellij.pom.Navigatable
+import org.apache.tools.ant.Project
+
+
+class RunConfiguration : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        println("RunConfigure Action Performed")
+
+        var project: com.intellij.openapi.project.Project? = e.project
+        var message: StringBuilder = StringBuilder(e.presentation.text + " Selected!")
+        var selElement: Editor? = e.getData(CommonDataKeys.EDITOR_EVEN_IF_INACTIVE) //NAVIGATABLE)
+        if (selElement != null) {
+            var selDocument: Document? = selElement.document
+            
+            message.append("\nSelected Element: ").append(selElement)
+        }
+        var title: String = e.presentation.description
+        Messages.showMessageDialog(
+            project,
+            message.toString(),
+            title,
+            Messages.getInformationIcon()
+        )
+    }
+
+    override fun update(e: AnActionEvent) {
+        println("update")
+        super.update(e)
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        println("getActionUpdateThread")
+        return super.getActionUpdateThread()
+    }
 }
+
