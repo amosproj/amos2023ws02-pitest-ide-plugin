c4ef4bbb85c44ed40b4b99a03abd7d72e9ec9d64
QW3RAT <luca.boehm@fau.de>
2023-11-28 15:34:48 +0100

    Added needed file to call ResultListener when PITest is run. Added logic to extract the Result class used by PITest directly instead of XML file
    Signed-off-by: QW3RAT <luca.boehm@fau.de>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/MyMutationResultListener.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/MyMutationResultListener.kt
@@ -3,18 +3,70 @@ package com.amos.pitmutationmate.pitmutationmate.reporting
 
 import org.pitest.mutationtest.ClassMutationResults
 import org.pitest.mutationtest.MutationResultListener
+import org.pitest.mutationtest.*
+import org.pitest.mutationtest.engine.MutationDetails;
+import java.util.*
+
 
 class MyMutationResultListener : MutationResultListener {
+    private  var resultData = ResultData()
+    private var mutators = mutableListOf<String>()
     override fun runStart() {
-        //TODO If needed: Initialization of logic or needed classes before the mutation testing run
+        resultData = ResultData()
     }
 
-    override fun handleMutationResult(results: ClassMutationResults?) {
+    override fun handleMutationResult(results: ClassMutationResults) {
         //TODO: Process and handle each mutation Result
+        for (result in results.mutations){
+            val details: MutationDetails = result.details
+            val status: DetectionStatus = result.status
+
+            resultData.addMutationResult(
+                MutationResult(
+                    detected = status.isDetected,
+                    status = status.name,
+                    numberOfTestsRun = result.numberOfTestsRun,
+                    sourceFile = details.filename,
+                    mutatedClass = details.className.nameWithoutPackage.asJavaName(),
+                    mutatedMethod = details.method,
+                    methodDescription = details.id.location.methodDesc,
+                    lineNumber = details.lineNumber,
+                    mutator = details.mutator,
+                    indexes = details.id.indexes.toList(),
+                    block = details.block,
+                    killingTest = result.killingTest,
+                    description = details.description
+                )
+            )
+        }
     }
 
     override fun runEnd() {
         //TODO If needed: Cleanup or final logic after the mutation test run
     }
+
+    data class ResultData(
+        val mutationResults: MutableList<MutationResult> = mutableListOf()
+    ) {
+        fun addMutationResult(mutationResult: MutationResult) {
+            mutationResults.add(mutationResult)
+        }
+    }
+
+    data class MutationResult(
+        val detected: Boolean,
+        val status: String,
+        val numberOfTestsRun: Int,
+        val sourceFile: String,
+        val mutatedClass: String,
+        val mutatedMethod: String,
+        val methodDescription: String,
+        val lineNumber: Int,
+        val mutator: String,
+        val indexes: List<Int>,
+        val block: Int,
+        val killingTest: Optional<String>,
+        val description: String
+    )
 
 }
