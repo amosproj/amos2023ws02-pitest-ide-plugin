2445d2f18d094c10b22ed0c2ac1762cec59bfaf8
Liam Fogarty <lfogarty9995@gmail.com>
2023-12-05 12:00:31 +0100

    display dark green for detected mutations, remove redundant code --signoff Co-authored-by: Tim Herzig <tim.herzig@hotmail.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/XMLListener.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/XMLListener.kt
@@ -15,8 +15,7 @@ class XMLListener(private var dir: Path, private var editor: Editor) {
     fun listen() {
         val pwd: String? = editor.project?.basePath?.let { Paths.get(it).toAbsolutePath().toString() }
         this.dir = Paths.get(pwd.toString(), this.dir.toString())
-
-        this.dir = Paths.get(pwd.toString(), "build","reports","pitest", "test", "mutations.xml")
+        //TODO: implement listening to changes in report directory
         loadResults()
         displayResults()
     }
@@ -28,7 +27,7 @@ class XMLListener(private var dir: Path, private var editor: Editor) {
 
     fun displayResults() {
         for (r in result.mutationResults) {
-            val color = if (r.detected) "light-green" else "dark-pink"
+            val color = if (r.detected) "dark-green" else "dark-pink"
             if (r.lineNumber > -1) {
                 println(r.toString())
                 HighlightGutterRenderer.GutterHighlighter.addBar(this.editor, color, r.lineNumber-1)
