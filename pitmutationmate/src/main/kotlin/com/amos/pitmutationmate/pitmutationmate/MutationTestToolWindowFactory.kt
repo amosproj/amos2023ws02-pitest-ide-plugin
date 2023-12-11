e7d53e964377f5aea10bcbfd4801c178b36a7b2e
Liam Fogarty <lfogarty9995@gmail.com>
2023-12-11 17:20:09 +0100

    display simple error message when there are no results to display
    Signed-off-by: Liam Fogarty <lfogarty9995@gmail.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/MutationTestToolWindowFactory.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/MutationTestToolWindowFactory.kt
@@ -11,11 +11,20 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
+import javax.swing.JPanel
 
-internal class MutationTestToolWindowFactory() : ToolWindowFactory, DumbAware {
+internal class MutationTestToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val latestPiTestReport = ContentFactory.getInstance().createContent(LatestPiTestReport(), "Latest Result", false)
+        //TODO: fetch most recent results to display (e.g. when opening up the editor and previous Pitest runs are saved)
+        val lastCoverageReport: XMLParser.CoverageReport? = null
+        val latestPiTestReport = if (lastCoverageReport == null) {
+            ContentFactory.getInstance().createContent(displayErrorMessage(), "Latest Result", false)
+        } else {
+            ContentFactory.getInstance().createContent(LatestPiTestReport(lastCoverageReport), "Latest Result", false)
+        }
         val table = ContentFactory.getInstance().createContent(JTreeTable(), "Mutationtest Coverage", false)
         val lineChart = ContentFactory.getInstance().createContent(LineGraph(), "Line Chart", false)
         val barChart = ContentFactory.getInstance().createContent(BarGraph(), "Bar Chart", false)
@@ -26,7 +35,18 @@ internal class MutationTestToolWindowFactory() : ToolWindowFactory, DumbAware {
         toolWindow.contentManager.addContent(barChart)
     }
 
-    fun updateReport(toolWindow: ToolWindow, coverageReport: XMLParser.CoverageReport) {
-        toolWindow.contentManager.getContent(0)?.component = LatestPiTestReport(coverageReport)
+    fun updateReport(toolWindow: ToolWindow, newCoverageReport: XMLParser.CoverageReport) {
+        toolWindow.contentManager.getContent(0)?.component = LatestPiTestReport(newCoverageReport)
+    }
+
+    private fun displayErrorMessage(): JPanel {
+        val panel = JPanel()
+
+        // Displaying an error message in the panel
+        val errorMessage = "No results to display yet."
+        val errorLabel = JBLabel(errorMessage)
+        panel.add(errorLabel)
+
+        return panel
     }
 }
