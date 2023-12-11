36366e3a986fb70a77326a6b94a6607fbb35fd1e
Liam Fogarty <lfogarty9995@gmail.com>
2023-12-11 16:16:46 +0100

    add data class for coverage results
    Signed-off-by: Liam Fogarty <lfogarty9995@gmail.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/XMLParser.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/reporting/XMLParser.kt
@@ -102,6 +102,15 @@ class XMLParser {
     }
 
     data class ResultData(
+        // placeholder field for coverage report results to be displayed in visualisation
+        val coverageReport: CoverageReport = CoverageReport(
+            lineCoveragePercentage = 80,
+            lineCoverageTextRatio = "160/200",
+            mutationCoveragePercentage = 50,
+            mutationCoverageTextRatio = "100/200",
+            testStrengthPercentage = 40,
+            testStrengthTextRatio = "80/200"
+        ),
         val mutationResults: MutableList<MutationResult> = mutableListOf()
     ) {
         fun addMutationResult(mutationResult: MutationResult) {
@@ -128,5 +137,14 @@ class XMLParser {
         val blocks: List<Int>,
         val killingTest: String,
         val description: String
+    )
+
+    data class CoverageReport(
+        val lineCoveragePercentage: Int,
+        val lineCoverageTextRatio: String,
+        val mutationCoveragePercentage: Int,
+        val mutationCoverageTextRatio: String,
+        val testStrengthPercentage: Int,
+        val testStrengthTextRatio: String
     )
 }
