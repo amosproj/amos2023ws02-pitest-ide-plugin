f61f9a85d15f3004dc0b11a6c46808bd76fe2224
brianneoberson <brianne.oberson@epfl.ch>
2023-12-11 17:21:54 +0100

    Remove println
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/GutterMarker.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/GutterMarker.kt
@@ -14,14 +14,12 @@ class GutterMarker : RunLineMarkerContributor() {
     override fun getInfo(psielement: PsiElement): Info? {
         val gutterIcon: Icon = AllIcons.General.ArrowRight
         if (psielement is PsiClass) {
-            println(psielement)
             val toolTipProvider: (PsiElement) -> String = { _ -> "Run PIT MutationMate on '${psielement.name}'" }
             val fqn = psielement.qualifiedName
             val action: Array<GutterAction?> = arrayOf(fqn?.let { GutterAction(it) })
             return Info(gutterIcon, action, toolTipProvider)
         }
         if (psielement is KtClass) {
-            println(psielement)
             val toolTipProvider: (PsiElement) -> String = { _ -> "Run PIT MutationMate on '${psielement.name}'" }
             val fqn = psielement.fqName.toString()
             val action: Array<GutterAction?> = arrayOf(GutterAction(fqn))
