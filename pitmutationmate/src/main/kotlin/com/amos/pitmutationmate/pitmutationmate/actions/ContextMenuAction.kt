bc1ee848a527adb595d3c60a66161947db55793a
brianneoberson <43467859+brianneoberson@users.noreply.github.com>
2023-12-12 17:07:59 +0100

    Feature/89 improve scope info (#126)
    * ContextMenuAction runs pitest on all classes of the file when clicked inside file tree structure.
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Comma seperate the classes
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Disable action if not clicked inside a class
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Adapt gutter marker for kotlin classes
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Adapt context menu action to work for kotlin files
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Remove println
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Fix lint
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Override actionupdatethread
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    * Replace println with logger.info
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    
    ---------
    
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>
    Co-authored-by: Felix-012 <84241137+Felix-012@users.noreply.github.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/ContextMenuAction.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/ContextMenuAction.kt
@@ -3,35 +3,82 @@
 
 package com.amos.pitmutationmate.pitmutationmate.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassOwner
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtClass
 
 class ContextMenuAction : RunConfigurationAction() {
+    private val logger = Logger.getInstance(ContextMenuAction::class.java)
     override fun actionPerformed(e: AnActionEvent) {
+        val editor = e.getData(CommonDataKeys.EDITOR)
         val psiFile = e.getData(CommonDataKeys.PSI_FILE)
-        println("ContextMenuAction: actionPerformed for file $psiFile")
-        val psiClasses = (psiFile as PsiClassOwner).classes
-        val fqns = mutableListOf<String>()
-        for (psiClass in psiClasses) {
-            val fqn = psiClass.qualifiedName
-            if (fqn != null) {
-                fqns.add(fqn)
-                println("ContextMenuAction: detected class '$fqn'")
+        if (e.place == "EditorPopup") {
+            logger.info("ContextMenuAction: actionPerformed in EditorPopup for file $psiFile")
+            val psiElement = psiFile?.findElementAt(editor?.caretModel!!.offset)
+            val selectedClass = findEnclosingClass(psiElement)
+            if (selectedClass != null) {
+                var classFQN = ""
+                if (selectedClass is PsiClass) {
+                    classFQN = selectedClass.qualifiedName.toString()
+                }
+                if (selectedClass is KtClass) {
+                    classFQN = selectedClass.fqName.toString()
+                }
+
+                logger.info("ContextMenuAction: selected class is $classFQN.")
+                updateAndExecuteRunConfig(classFQN, e.project!!, editor)
             }
         }
-        val editor = e.getData(CommonDataKeys.EDITOR)
-        updateAndExecuteRunConfig(fqns.first(), e.project!!, editor)
+        if (e.place == "ProjectViewPopup") {
+            logger.info("ContextMenuAction: actionPerformed in ProjectViewPopup for file $psiFile")
+            val psiClasses = (psiFile as PsiClassOwner).classes
+            var classFQNs: String = ""
+            for (psiClass in psiClasses) {
+                val fqn = psiClass.qualifiedName
+                if (fqn != null) {
+                    classFQNs = if (classFQNs != "") {
+                        "$classFQNs,$fqn"
+                    } else {
+                        fqn
+                    }
+                }
+            }
+            logger.info("ContextMenuAction: selected classes are $classFQNs.")
+            updateAndExecuteRunConfig(classFQNs, e.project!!, editor)
+        }
     }
 
     override fun update(e: AnActionEvent) {
-        val file: VirtualFile? = e.dataContext.getData("virtualFile") as VirtualFile?
-        val shouldEnable: Boolean = checkCondition(file)
+        val shouldEnable: Boolean = checkCondition(e)
         e.presentation.isEnabled = shouldEnable
     }
 
-    private fun checkCondition(file: VirtualFile?): Boolean {
-        return file != null && (file.name.endsWith(".java") || file.name.endsWith(".kt"))
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+
+    private fun checkCondition(e: AnActionEvent): Boolean {
+        val psiFile = e.getData(CommonDataKeys.PSI_FILE)
+        val validFile = psiFile != null && (psiFile.name.endsWith(".java") || psiFile.name.endsWith(".kt"))
+        if (e.place == "EditorPopup") {
+            val editor = e.getData(CommonDataKeys.EDITOR)
+            val psiElement = psiFile?.findElementAt(editor?.caretModel!!.offset)
+            val validClass = (findEnclosingClass(psiElement) != null)
+            return validFile && validClass
+        }
+        return validFile
+    }
+
+    private fun findEnclosingClass(psiElement: PsiElement?): PsiElement? {
+        var currentElement: PsiElement? = psiElement
+        while (currentElement != null && currentElement !is PsiClass && currentElement !is KtClass) {
+            currentElement = currentElement.parent
+        }
+        return currentElement
     }
 }
