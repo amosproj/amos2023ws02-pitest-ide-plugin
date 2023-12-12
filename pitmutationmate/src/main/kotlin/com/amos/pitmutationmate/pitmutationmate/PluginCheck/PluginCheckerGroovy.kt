dfb004ba33671b1386a1dacbfd50f4d347078f6e
nikomall34 <niko.malliaros@fau.de>
2023-12-12 22:18:41 +0100

    Added a groovy and a kotlin parser to parse the build.gradle and the build.gradle.kts files
    Signed-off-by: nikomall34 <niko.malliaros@fau.de>

---
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/PluginCheck/PluginCheckerGroovy.kt
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023
+
+package com.amos.pitmutationmate.pitmutationmate.PluginCheck
+
+import org.codehaus.groovy.ast.CodeVisitorSupport
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+
+class PluginCheckerGroovy : CodeVisitorSupport() {
+
+    var pitestPluginAvailable = false
+    var companionPluginAvailable = false
+
+    override fun visitMethodCallExpression(call: MethodCallExpression?) {
+        val method = call?.methodAsString
+        if(method.equals("plugins") || method.equals("apply") || method.equals("version")){
+            super.visitMethodCallExpression(call)
+        } else if(method.equals("id")){
+            if (call != null) {
+                val pluginName = call.arguments.text
+                if(pluginName.contains("pitest")){
+                    pitestPluginAvailable = true
+                }
+                if(pluginName.contains("io.github.amosproj.pitmutationmate.override")){
+                    companionPluginAvailable = true
+                }
+            }
+        }
+    }
+}
