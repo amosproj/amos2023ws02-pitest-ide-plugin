c91d5deee9274a14e390e5f4c4e74eaa2692c476
Tim Herzig <tim.herzig@hotmail.com>
2024-01-08 16:19:57 +0100

    remove wildcard import
    Signed-off-by: Tim Herzig <tim.herzig@hotmail.com>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/MutationTestToolWindowFactory.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/MutationTestToolWindowFactory.kt
@@ -3,7 +3,9 @@
 package com.amos.pitmutationmate.pitmutationmate
 
 import com.amos.pitmutationmate.pitmutationmate.reporting.XMLParser
-import com.amos.pitmutationmate.pitmutationmate.visualization.*
+import com.amos.pitmutationmate.pitmutationmate.visualization.BarGraph
+import com.amos.pitmutationmate.pitmutationmate.visualization.LineGraph
+import com.amos.pitmutationmate.pitmutationmate.visualization.PiTestReports
 import com.amos.pitmutationmate.pitmutationmate.visualization.treetable.JTreeTable
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
