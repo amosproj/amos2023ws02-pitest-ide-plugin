bdc4b8d6c849e80f5a8ff81520fd3074b2a3e064
Lennart Heimbs <lennart@heimbs.me>
2024-01-08 19:27:32 +0100

    bundle icons in class
    Signed-off-by: Lennart Heimbs <lennart@heimbs.me>

---
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/icons/Icons.kt
@@ -0,0 +1,14 @@
+package com.amos.pitmutationmate.pitmutationmate.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val Logo16 = IconLoader.getIcon("icons/pluginIcon16.svg", javaClass)
+
+    @JvmField
+    val Logo13 = IconLoader.getIcon("icons/pluginIcon13.svg", javaClass)
+
+    @JvmField
+    val Logo12 = IconLoader.getIcon("icons/pluginIcon12.svg", javaClass)
+}
