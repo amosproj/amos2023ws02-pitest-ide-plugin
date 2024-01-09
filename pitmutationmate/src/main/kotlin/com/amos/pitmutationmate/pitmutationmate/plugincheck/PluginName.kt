5cef44621b2f157e313139596cdc33a187996f14
Lennart Heimbs <lennart@heimbs.me>
2024-01-09 14:36:10 +0100

    adapt to changed plugin id
    Signed-off-by: Lennart Heimbs <lennart@heimbs.me>

---
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/plugincheck/PluginName.kt
@@ -0,0 +1,6 @@
+package com.amos.pitmutationmate.pitmutationmate.plugincheck
+
+object PluginName {
+    const val PITEST = "pitest"
+    const val COMPANION = "io.github.amos-pitmutationmate.pitmutationmate.override"
+}
