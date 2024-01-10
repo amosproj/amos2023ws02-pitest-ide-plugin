e7a00e442140d122f72a17d8b216d89058666460
nikomall34 <niko.malliaros@fau.de>
2024-01-10 10:21:21 +0100

    removed the information for publishing a gradle plugin
    Signed-off-by: nikomall34 <niko.malliaros@fau.de>

--- coverage-plugin/build.gradle.kts
+++ coverage-plugin/build.gradle.kts
@@ -3,11 +3,8 @@
 
 plugins {
     kotlin("jvm") version "1.9.21"
-    `java-gradle-plugin`
     `maven-publish`
     idea
-
-    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "io.github.amosproj"
@@ -28,26 +25,4 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
-}
-
-gradlePlugin {
-    website = "https://github.com/amosproj/amos2023ws02-pitest-ide-plugin"
-    vcsUrl = "https://github.com/amosproj/amos2023ws02-pitest-ide-plugin.git"
-
-    plugins {
-        create("pitmutationmate-coverage-plugin") {
-            id = "io.github.amosproj.pitmutationmate.coverage"
-            displayName = "PITMutationMate Coverage Pitest Plugin"
-            description = "A plugin that implements the MutationResultListener class from pitest " +
-                "The custom MutationResultListener provides the coverage information in a handy way " +
-                "for the PITMutationMate  Intellij plugin."
-            tags =
-                listOf(
-                    "pitmutationmate", "pitest", "mutation", "mutation testing", "mutation testing tool",
-                    "mutation testing framework", "mutation testing gradle plugin", "mutation testing ide plugin",
-                    "mutation testing intellij plugin", "mutation testing intellij idea plugin"
-                )
-            implementationClass = "io.github.amosproj.pitmutationmate.coverage.CoverageResultListenerFactory"
-        }
-    }
 }
