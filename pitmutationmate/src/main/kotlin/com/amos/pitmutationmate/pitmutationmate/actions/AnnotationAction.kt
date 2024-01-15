b16eb7f00ee719ac8a7e6f5df8b05de1f8a5036c
brianneoberson <brianne.oberson@epfl.ch>
2024-01-15 15:43:12 +0100

    Enable annotations after pitest run
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/AnnotationAction.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/AnnotationAction.kt
@@ -7,8 +7,6 @@ import com.amos.pitmutationmate.pitmutationmate.editor.PluginState
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.psi.PsiFile
 
 class AnnotationAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -17,9 +15,6 @@ class AnnotationAction : AnAction() {
         // Trigger a reanalysis of the file to refresh the annotations
         // by using the DaemonCodeAnalyzer
         val project = e.project
-        val psiFile: PsiFile? = e.getData(CommonDataKeys.PSI_FILE)
-        psiFile?.let {
-            DaemonCodeAnalyzer.getInstance(project).restart(psiFile)
-        }
+        DaemonCodeAnalyzer.getInstance(project).restart()
     }
 }
