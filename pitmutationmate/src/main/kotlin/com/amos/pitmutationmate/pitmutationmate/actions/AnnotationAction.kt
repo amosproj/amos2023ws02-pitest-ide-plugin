469b3c43dfadebe2f38ef1a1d5e575f44d2b2abf
brianneoberson <brianne.oberson@epfl.ch>
2024-01-15 10:31:23 +0100

    Refactor AnnotationAction
    Signed-off-by: brianneoberson <brianne.oberson@epfl.ch>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/AnnotationAction.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/AnnotationAction.kt
@@ -6,21 +6,20 @@ package com.amos.pitmutationmate.pitmutationmate.actions
 import com.amos.pitmutationmate.pitmutationmate.editor.PluginState
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
-import com.intellij.openapi.actionSystem.CommonDataKeys
 
 class AnnotationAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         PluginState.isAnnotatorEnabled = true
+
+        // Trigger a reanalysis of the file to refresh the annotations
+        // by using the PsiDocumentManager to commit the changes to the document.
         val project = e.project
         val documentManager = PsiDocumentManager.getInstance(project!!)
-
-        // Use CommonDataKeys.PSI_FILE to get the PsiFile
         val psiFile: PsiFile? = e.getData(CommonDataKeys.PSI_FILE)
-
         psiFile?.let {
-            // Get the current editor's document and commit the changes
             documentManager.commitDocument(it.viewProvider.document)
         }
     }
