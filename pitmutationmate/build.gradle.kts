6098d0f85785b1803ae673e225a883e97f293234
nikomall34 <niko.malliaros@fau.de>
2024-01-19 22:59:03 +0100

    Updated intellij plugin
    Signed-off-by: nikomall34 <niko.malliaros@fau.de>

--- pitmutationmate/build.gradle.kts
+++ pitmutationmate/build.gradle.kts
@@ -4,7 +4,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.17.0"
     id("info.solidsoft.pitest") version "1.15.0"
 }
 
