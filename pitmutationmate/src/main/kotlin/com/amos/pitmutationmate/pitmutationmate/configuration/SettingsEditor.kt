7bd41990c78724b2273ac32a952d30a74062cf71
Liam Fogarty <fogarty@ztm.de>
2024-01-22 12:16:04 +0100

    add text field for scope information

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/configuration/SettingsEditor.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/configuration/SettingsEditor.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.ui.TextFieldWithHistory
+import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.FormBuilder
 import javax.swing.JCheckBox
 import javax.swing.JComponent
@@ -17,6 +18,7 @@ class SettingsEditor : SettingsEditor<RunConfiguration>() {
     private val gradleTaskField: TextFieldWithHistory = TextFieldWithHistory()
     private val gradleExecutableField: TextFieldWithBrowseButton = TextFieldWithBrowseButton()
     private val overwriteCheckbox: JCheckBox = JCheckBox("No Overwrite")
+    private val scopeField: JBTextField = JBTextField()
 
     init {
         gradleTaskField.text = "pitest"
@@ -26,21 +28,25 @@ class SettingsEditor : SettingsEditor<RunConfiguration>() {
             null,
             FileChooserDescriptorFactory.createSingleFileDescriptor()
         )
+        scopeField.getEmptyText().setText("com.myproj.package1.classA,com.myproj.package1.classB,com.myproj.package2.classC,...")
         myPanel = FormBuilder.createFormBuilder()
             .addLabeledComponent("Overwrite behavior", overwriteCheckbox)
             .addLabeledComponent("Gradle task", gradleTaskField)
             .addLabeledComponent("Gradle script", gradleExecutableField)
+            .addLabeledComponent("Scope", scopeField)
             .panel
     }
 
     override fun resetEditorFrom(runConfiguration: RunConfiguration) {
         overwriteCheckbox.isSelected = runConfiguration.overwriteScope
+        scopeField.text = runConfiguration.classFQN
         gradleTaskField.text = runConfiguration.taskName
         runConfiguration.gradleExecutable.also { gradleExecutableField.text = it ?: "" }
     }
 
     override fun applyEditorTo(runConfiguration: RunConfiguration) {
         runConfiguration.overwriteScope = overwriteCheckbox.isSelected
+        runConfiguration.classFQN = scopeField.text
         runConfiguration.taskName = gradleTaskField.text
         runConfiguration.gradleExecutable = gradleExecutableField.text
     }
