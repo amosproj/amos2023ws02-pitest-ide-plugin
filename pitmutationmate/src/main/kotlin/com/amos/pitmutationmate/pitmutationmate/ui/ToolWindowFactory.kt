9473f8663e7432b454b7f30a97734d1ab43b1de0
QW3RAT <luca.boehm@fau.de>
2024-01-23 11:10:20 +0100

    Removed unused import

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/ui/ToolWindowFactory.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/ui/ToolWindowFactory.kt
@@ -15,7 +15,6 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.content.ContentFactory
 
 internal class ToolWindowFactory : ToolWindowFactory, DumbAware {
