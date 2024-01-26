8ed2c3b4d88907610f7dff03a77a0bd511610976
nikomall34 <niko.malliaros@fau.de>
2024-01-26 01:10:28 +0100

    fixed import order
    Signed-off-by: nikomall34 <niko.malliaros@fau.de>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/ContextMenuAction.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/actions/ContextMenuAction.kt
@@ -14,11 +14,11 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassOwner
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.psi.KtClass
 import java.io.File
+import org.jetbrains.kotlin.psi.KtClass
 
 class ContextMenuAction : RunConfigurationAction() {
     private val logger = Logger.getInstance(ContextMenuAction::class.java)
