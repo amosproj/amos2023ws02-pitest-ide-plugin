5ce96bcccfc7d48ffb3f098baf1a638a9f643c18
Liam Fogarty <fogarty@ztm.de>
2024-01-30 17:21:29 +0100

    set argument of StoredProperty delegate correctly
    Co-authored-by: Brianne Oberson <brianne.oberson@gmail.com>
    Signed-off-by: Liam Fogarty <fogarty@ztm.de>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/configuration/RunConfigurationOptions.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/configuration/RunConfigurationOptions.kt
@@ -7,21 +7,21 @@ import com.intellij.execution.configurations.RunConfigurationOptions
 import com.intellij.openapi.components.StoredProperty
 
 class RunConfigurationOptions : RunConfigurationOptions() {
-    private var taskNameOption: StoredProperty<String?> = string("").provideDelegate(this, "gradle.task")
+    private var taskNameOption: StoredProperty<String?> = string("").provideDelegate(this, "taskName")
     var taskName: String?
         get() = taskNameOption.getValue(this)
         set(value) {
             taskNameOption.setValue(this, value)
         }
 
-    private var classFQNOption: StoredProperty<String?> = string("").provideDelegate(this, "gradle.task")
+    private var classFQNOption: StoredProperty<String?> = string("").provideDelegate(this, "classFQN")
     var classFQN: String?
         get() = classFQNOption.getValue(this)
         set(value) {
             classFQNOption.setValue(this, value)
         }
 
-    private val gradleExecutableOption: StoredProperty<String?> = string("").provideDelegate(this, "gradle.executable")
+    private val gradleExecutableOption: StoredProperty<String?> = string("").provideDelegate(this, "gradleExecutable")
     var gradleExecutable: String?
         get() = gradleExecutableOption.getValue(this)
         set(value) {
