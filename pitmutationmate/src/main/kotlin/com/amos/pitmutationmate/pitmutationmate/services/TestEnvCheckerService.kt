e1f4cd19f138e972fa9655d0ccbdf6fdc1781fc9
Lennart Heimbs <lennart@heimbs.me>
2024-02-03 15:37:33 +0100

    use gradle provided test paths to check for test files
    Shoud be more robust with gradle projects.
    
    Signed-off-by: Lennart Heimbs <lennart@heimbs.me>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/services/TestEnvCheckerService.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/services/TestEnvCheckerService.kt
@@ -5,9 +5,12 @@ package com.amos.pitmutationmate.pitmutationmate.services
 
 import com.amos.pitmutationmate.pitmutationmate.utils.Utils
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassOwner
+import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.psi.KtClass
 import java.io.File
 
@@ -15,25 +18,54 @@ import java.io.File
 class TestEnvCheckerService(private val project: Project) {
 
     fun isTestFile(file: File): Boolean {
-        val psiFile = Utils.getPsiFileFromPath(project, file.path)
-        val psiClasses = (psiFile as PsiClassOwner).classes
+        if (project.service<PluginCheckerService>().getTestDirectories().isEmpty()) {
+            val psiFile = Utils.getPsiFileFromPath(project, file.path)
+            val psiClasses = (psiFile as PsiClassOwner).classes
 
-        for (psiClass in psiClasses) {
-            if (isPsiTestClass(psiClass)) {
+            for (psiClass in psiClasses) {
+                if (isPsiTestClass(psiClass)) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        project.service<PluginCheckerService>().getTestDirectories().forEach { testDir ->
+            if (file.path.startsWith(testDir.path)) {
                 return true
             }
         }
         return false
     }
 
-    // TODO This should be adjusted with a more thorough test
     fun isPsiTestClass(psiClass: PsiClass): Boolean {
+        if (project.service<PluginCheckerService>().getTestDirectories().isNotEmpty()) {
+            val file = getFileFromPsiFile(psiClass.containingFile)
+            if (file != null) {
+                return isTestFile(file)
+            }
+        }
         val fqn = psiClass.qualifiedName.toString()
         return fqn.endsWith("Test")
     }
 
     fun isKtTestClass(ktClass: KtClass): Boolean {
+        if (project.service<PluginCheckerService>().getTestDirectories().isNotEmpty()) {
+            val file = getFileFromPsiFile(ktClass.containingFile)
+            if (file != null) {
+                return isTestFile(file)
+            }
+            return false
+        }
         val fqn = ktClass.fqName.toString()
         return fqn.endsWith("Test")
+    }
+
+    private fun getFileFromPsiFile(psiFile: PsiFile): File? {
+        val file: VirtualFile? = psiFile.virtualFile
+        if (file != null && file.isInLocalFileSystem && file.canonicalPath != null) {
+            return File(file.canonicalPath!!)
+        }
+        return null
     }
 }
