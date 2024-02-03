d4b16b39711cefd20657e29a0ffc1a365c2c6cd5
Lennart Heimbs <lennart@heimbs.me>
2024-02-03 16:47:21 +0100

    remove all plugin check file parsing classes
    Signed-off-by: Lennart Heimbs <lennart@heimbs.me>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/plugincheck/PluginName.kt
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// SPDX-FileCopyrightText: 2023 Lennart Heimbs
-
-package com.amos.pitmutationmate.pitmutationmate.plugincheck
-
-object PluginName {
-    const val PITEST = "pitest"
-    const val COMPANION = "io.github.amos-pitmutationmate.pitmutationmate.override"
-}
