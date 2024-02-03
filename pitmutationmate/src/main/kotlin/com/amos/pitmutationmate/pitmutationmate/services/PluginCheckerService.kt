db5a814c8c5a9e6c55a45ae34e27067fc3989d12
Lennart Heimbs <lennart@heimbs.me>
2024-02-03 15:05:08 +0100

    revamp the entire plugin checking part.
    This completely dumps any parsing of files in favor of a gradle task
    registered by the compagnion plugin which existence signals the
    presence of the compagnion. In turn the compagnion task check for the
    solidsoft and driodsondriods plugins and prints then in json form
    in the gradle task. If we then parse the output of that task
    we get the presence of the pitest plugins.
    
    The gradle task is also able to get the available android build types.
    Sadly i still haven't found a way to get the currently set build type.
    I think that information mainly lives in the intellij android plugin...
    
    Signed-off-by: Lennart Heimbs <lennart@heimbs.me>

--- pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/services/PluginCheckerService.kt
+++ pitmutationmate/src/main/kotlin/com/amos/pitmutationmate/pitmutationmate/services/PluginCheckerService.kt
@@ -3,184 +3,218 @@
 
 package com.amos.pitmutationmate.pitmutationmate.services
 
-import com.amos.pitmutationmate.pitmutationmate.plugincheck.PluginCheckerGroovy
-import com.amos.pitmutationmate.pitmutationmate.plugincheck.PluginCheckerKotlin
-import com.amos.pitmutationmate.pitmutationmate.ui.ToolWindowFactory
+import com.amos.pitmutationmate.pitmutationmate.plugincheck.PluginCheckData
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.psi.PsiManager
-import com.intellij.util.ui.UIUtil
-import org.apache.commons.io.IOUtils
-import org.codehaus.groovy.ast.builder.AstBuilder
-import java.io.ByteArrayInputStream
-import java.io.InputStream
+import kotlinx.serialization.json.Json
+import org.gradle.tooling.GradleConnector
+import org.gradle.tooling.ProjectConnection
+import org.gradle.tooling.model.GradleProject
+import org.gradle.tooling.model.idea.IdeaProject
+import org.jetbrains.kotlin.idea.configuration.isMavenized
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.nio.charset.StandardCharsets
 
 @Service(Service.Level.PROJECT)
 class PluginCheckerService(private val project: Project) {
     private val logger = Logger.getInstance(this::class.java)
 
-    private var isKotlinOrGroovy: BuildFile = BuildFile.NONE
-    private var kotlinPluginsAvailability = PluginsAvailability()
-    private var groovyPluginsAvailability = PluginsAvailability()
+    private var isCompanionPluginAvailable = false
+    private var pluginCheckData: PluginCheckData? = null
+    private var buildFiles = mutableListOf<File>()
+    private var testDirectories = mutableListOf<File>()
+    private var sourceDirectories = mutableListOf<File>()
 
-    fun checkPlugins(files: Collection<VirtualFile>) {
-        val kotlinPlugins = PluginsAvailability()
-        val groovyPlugins = PluginsAvailability()
-
-        // iterate over given files
-        for (file in files) {
-            if (!file.exists()) {
-                logger.info("File ${file.path} does not exist. Not checking for plugins.")
-                continue
-            }
-
-            when (file.name) {
-                "build.gradle.kts" -> {
-                    checkKotlinBuildFile(file, kotlinPlugins)
-                    kotlinPluginsAvailability = kotlinPlugins
-                    isKotlinOrGroovy = BuildFile.KOTLIN
-                }
-
-                "build.gradle" -> {
-                    checkGroovyBuildFile(file, groovyPlugins)
-                    groovyPluginsAvailability = groovyPlugins
-                    isKotlinOrGroovy = BuildFile.GROOVY
-                }
-
-                else -> {
-                    logger.info("File ${file.path} is not a build file. Not checking for plugins.")
-                    continue
-                }
-            }
+    fun checkPlugins() {
+        if (project.isMavenized) {
+            isCompanionPluginAvailable = true
+            pluginCheckData = PluginCheckData(
+                pitestPluginApplied = true,
+                androidPitestPluginApplied = true,
+                androidPluginApplied = true,
+                androidBuildTypes = listOf()
+            )
+        } else {
+            checkPluginsGradle()
         }
-        fillToolWindow()
+    }
+
+    fun checkBuildEnvironment() {
+        if (project.isMavenized) {
+            checkBuildEnvironmentMaven()
+        } else {
+            checkBuildEnvironmentGradle()
+        }
     }
 
     fun getErrorMessage(withHeader: Boolean = true): String? {
         var errorMessage = ""
 
-        val pluginsAvailability = PluginsAvailability()
-        pluginsAvailability.pitestPluginAvailable = kotlinPluginsAvailability.pitestPluginAvailable || groovyPluginsAvailability.pitestPluginAvailable
-        pluginsAvailability.companionPluginAvailable = kotlinPluginsAvailability.companionPluginAvailable || groovyPluginsAvailability.companionPluginAvailable
-
-        val pitestPluginString = when (isKotlinOrGroovy) {
-            BuildFile.GROOVY -> GROOVY_PITEST_PLUGIN
-            else -> KOTLIN_PITEST_PLUGIN
-        }
-        val overridePluginString = when (isKotlinOrGroovy) {
-            BuildFile.GROOVY -> GROOVY_OVERRIDE_PLUGIN
-            else -> KOTLIN_OVERRIDE_PLUGIN
-        }
-        val buildFileName = when (isKotlinOrGroovy) {
-            BuildFile.GROOVY -> GROOVY_BUILD_FILE
-            else -> KOTLIN_BUILD_FILE
-        }
-
-        if (!pluginsAvailability.pitestPluginAvailable) {
+        if (!isCompanionPluginAvailable) {
             if (errorMessage.isNotEmpty()) errorMessage += "\n"
-            errorMessage += String.format(
-                ERROR_MESSAGE_PITEST_PLUGIN_MISSING,
-                buildFileName,
-                pitestPluginString
+            errorMessage += ERROR_MESSAGE_COMPANION_PLUGIN_MISSING.format(
+                getBuildFileName(),
+                getCompanionPluginString()
             )
         }
-        if (!pluginsAvailability.companionPluginAvailable) {
+
+        // Check if any of the pitest plugins are missing
+        else if (pluginCheckData == null || (!pluginCheckData!!.pitestPluginApplied && !pluginCheckData!!.androidPitestPluginApplied)) {
             if (errorMessage.isNotEmpty()) errorMessage += "\n"
-            errorMessage += String.format(
-                ERROR_MESSAGE_COMPANION_PLUGIN_MISSING,
-                buildFileName,
-                overridePluginString
-            )
+            val (buildFile, pluginString, pluginUrl) = getPitestPluginName()
+            errorMessage += ERROR_MESSAGE_PITEST_PLUGIN_MISSING.format(buildFile, pluginString, pluginUrl)
         }
 
         if (errorMessage.isNotEmpty()) {
-            val header = if (withHeader) "<h1>Configuration error:</h1><br/>" else ""
-            val font = UIUtil.getToolbarFont()
-            return """<html>
-                <body style="font-family: '${font.family}'; font-size: 12px; margin: 15px;">
-                    $header${errorMessage.replace("\n", "<br/>")}
-                </body>
-            </html>
-            """.trimIndent()
+            if (withHeader) {
+                return "$ERROR_MESSAGE_TITLE\n\n$errorMessage"
+            }
+            return errorMessage
         }
         return null
     }
 
-    private fun fillToolWindow() {
-        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ToolWindowFactory.ID)
-        if (toolWindow != null) {
-            val errorMessage = getErrorMessage()
-            if (!errorMessage.isNullOrEmpty()) {
-                ToolWindowFactory.Util.initiateWithConfigError(errorMessage, toolWindow)
-            } else {
-                ToolWindowFactory.Util.initiateWithData(toolWindow, project)
+    fun getTestDirectories(): List<File> {
+        return testDirectories
+    }
+
+    fun getBuildTypes(): List<String> {
+        return pluginCheckData?.androidBuildTypes ?: emptyList()
+    }
+
+    private fun checkPluginsGradle() {
+        val projectDir = File(project.basePath ?: "")
+
+        GradleConnector.newConnector().forProjectDirectory(projectDir).connect().use { connection ->
+            // check if the pitmutationmateStatusCheck task is available
+            checkForCompanionPlugin(connection)
+            if (!isCompanionPluginAvailable) {
+                logger.info("Companion plugin is not available. Not checking for plugins.")
+                return
             }
+            // check if the pitest plugin is available
+            checkForPlugins(connection)
         }
     }
 
-    private fun checkKotlinBuildFile(kotlinBuildFile: VirtualFile, kotlinPlugins: PluginsAvailability) {
-        thisLogger().debug("checking kotlin plugins for $kotlinBuildFile")
-        val psiFile = PsiManager.getInstance(project).findFile(kotlinBuildFile)
-        val kotlinChecker = PluginCheckerKotlin()
-        psiFile?.node?.psi?.accept(kotlinChecker)
-
-        if (!kotlinPlugins.pitestPluginAvailable) {
-            kotlinPlugins.pitestPluginAvailable = kotlinChecker.pitestPluginAvailable
-        }
-        if (!kotlinPlugins.companionPluginAvailable) {
-            kotlinPlugins.companionPluginAvailable = kotlinChecker.companionPluginAvailable
-        }
-    }
-
-    private fun checkGroovyBuildFile(groovyBuildFile: VirtualFile, groovyPlugins: PluginsAvailability) {
-        thisLogger().debug("checking groovy plugins for $groovyBuildFile")
-        val builder = AstBuilder()
-        val nodes = builder.buildFromString(
-            IOUtils.toString(
-                getInputStreamFromVirtualFile(groovyBuildFile),
-                "UTF-8"
+    private fun checkBuildEnvironmentMaven() {
+        val projectDir = File(project.basePath ?: "")
+        val pomFile = File(projectDir, "pom.xml")
+        buildFiles.add(pomFile)
+        // add project base path + src/test/{java/kotlin} to testDirectories
+        testDirectories.addAll(
+            listOf(
+                File(projectDir, "src/test/java"),
+                File(projectDir, "src/test/kotlin")
             )
         )
-        val groovyChecker = PluginCheckerGroovy()
-        for (node in nodes) {
-            if (node::class.java.declaredMethods.any { it.name == "visit" }) {
-                node.visit(groovyChecker)
+    }
+
+    private fun checkBuildEnvironmentGradle() {
+        val projectDir = File(project.basePath ?: "")
+
+        GradleConnector.newConnector().forProjectDirectory(projectDir).connect().use { connection ->
+            getBuildFiles(connection)
+        }
+    }
+
+    private fun getBuildFiles(connection: ProjectConnection) {
+        try {
+            val ideaProject = connection.getModel(IdeaProject::class.java)
+            ideaProject.modules.forEach { module ->
+                buildFiles.add(module.gradleProject.buildScript.sourceFile)
+                module.contentRoots.forEach { contentRoot ->
+                    sourceDirectories.addAll(contentRoot.sourceDirectories.map { it.directory })
+                    testDirectories.addAll(contentRoot.testDirectories.map { it.directory })
+                }
+            }
+        } catch (e: Exception) {
+            logger.warn("Could not get gradle script file", e)
+        }
+    }
+
+    private fun checkForCompanionPlugin(connection: ProjectConnection) {
+        val project = connection.getModel(GradleProject::class.java)
+        // Recursively search for the task in the project and its subprojects
+        isCompanionPluginAvailable = findTaskRecursively(project)
+    }
+
+    private fun checkForPlugins(connection: ProjectConnection) {
+        val outputStream = ByteArrayOutputStream()
+        connection.newBuild().forTasks(TASK_NAME).setStandardOutput(outputStream).run()
+        var output = outputStream.toString(StandardCharsets.UTF_8.name())
+        logger.trace("Got output from check task: $output")
+        // remove content until the first { and from behind until the last }
+        output = "{" + output.substringAfter("{").substringBeforeLast("}") + "}"
+
+        // Deserialize the JSON string into a Kotlin object
+        pluginCheckData = Json.decodeFromString<PluginCheckData>(output)
+        logger.debug("Got plugin check data: $pluginCheckData")
+    }
+
+    private fun findTaskRecursively(project: GradleProject): Boolean {
+        // Check if the current project contains the task
+        val taskExistsInCurrentProject = project.tasks.any { it.name == TASK_NAME }
+
+        if (taskExistsInCurrentProject) {
+            return true
+        }
+        // Recursively check subprojects
+        return project.children.any { findTaskRecursively(it) }
+    }
+
+    private fun getBuildFileName(): String {
+        return buildFiles.firstOrNull()?.name ?: "Unknown build file"
+    }
+
+    private fun getCompanionPluginString(): String {
+        val buildFile = getBuildFileName()
+        return if (buildFile.endsWith(KOTLIN_BUILD_FILE)) {
+            KOTLIN_OVERRIDE_PLUGIN
+        } else if (buildFile.endsWith(GROOVY_BUILD_FILE)) {
+            GROOVY_OVERRIDE_PLUGIN
+        } else {
+            ""
+        }
+    }
+
+    private fun getPitestPluginName(): List<String> {
+        val isAndroid = pluginCheckData?.androidPitestPluginApplied ?: false
+        val buildFile = getBuildFileName()
+
+        return if (isAndroid) {
+            when (buildFile) {
+                KOTLIN_BUILD_FILE -> listOf(buildFile, KOTLIN_PITEST_ANDROID_PLUGIN, PITEST_ANDROID_URL)
+                GROOVY_BUILD_FILE -> listOf(buildFile, GROOVY_PITEST_ANDROID_PLUGIN, PITEST_ANDROID_URL)
+                else -> listOf(buildFile, PITEST_ANDROID_PLUGIN, PITEST_ANDROID_URL)
+            }
+        } else {
+            when (buildFile) {
+                KOTLIN_BUILD_FILE -> listOf(buildFile, KOTLIN_PITEST_PLUGIN, PITEST_URL)
+                GROOVY_BUILD_FILE -> listOf(buildFile, GROOVY_PITEST_PLUGIN, PITEST_URL)
+                else -> listOf(buildFile, PITEST_PLUGIN, PITEST_URL)
             }
         }
-        if (!groovyPlugins.pitestPluginAvailable) {
-            groovyPlugins.pitestPluginAvailable = groovyChecker.pitestPluginAvailable
-        }
-        if (!groovyPlugins.companionPluginAvailable) {
-            groovyPlugins.companionPluginAvailable = groovyChecker.companionPluginAvailable
-        }
-    }
-
-    private fun getInputStreamFromVirtualFile(virtualFile: VirtualFile): InputStream? {
-        val document = FileDocumentManager.getInstance().getDocument(virtualFile)
-        return document?.let {
-            ByteArrayInputStream(it.text.toByteArray())
-        }
-    }
-
-    private class PluginsAvailability {
-        var pitestPluginAvailable: Boolean = false
-        var companionPluginAvailable: Boolean = false
     }
 
     companion object {
         private const val KOTLIN_BUILD_FILE = "build.gradle.kts"
         private const val GROOVY_BUILD_FILE = "build.gradle"
 
-        private const val KOTLIN_PITEST_PLUGIN = "id(\"info.solidsoft.pitest\") version \"x.y.z\""
+        private const val PITEST_ANDROID_PLUGIN = "pl.droidsonroids.pitest"
+        private const val PITEST_PLUGIN = "info.solidsoft.pitest"
+        private const val PITEST_ANDROID_URL = "https://plugins.gradle.org/plugin/pl.droidsonroids.pitest"
+        private const val PITEST_URL = "https://plugins.gradle.org/plugin/info.solidsoft.pitest"
+
+        private const val KOTLIN_PITEST_PLUGIN = "id(\"$PITEST_PLUGIN\") version \"x.y.z\""
+        private const val KOTLIN_PITEST_ANDROID_PLUGIN = "id(\"$PITEST_ANDROID_PLUGIN\") version \"x.y.z\""
         private const val KOTLIN_OVERRIDE_PLUGIN =
             "id(\"io.github.amos-pitmutationmate.pitmutationmate.override\") version \"1.2\""
 
-        private const val GROOVY_PITEST_PLUGIN = "id 'info.solidsoft.pitest' version 'x.y.z'"
+        private const val GROOVY_PITEST_PLUGIN = "id '$PITEST_PLUGIN' version 'x.y.z'"
+        private const val GROOVY_PITEST_ANDROID_PLUGIN = "id '$PITEST_ANDROID_PLUGIN' version 'x.y.z'"
         private const val GROOVY_OVERRIDE_PLUGIN =
             "id \"io.github.amos-pitmutationmate.pitmutationmate.override\" version \"1.2\""
 
@@ -188,15 +222,13 @@ class PluginCheckerService(private val project: Project) {
         private val ERROR_MESSAGE_PITEST_PLUGIN_MISSING = """<b>The pitest gradle Plugin is missing.</b>
             <p>Please add a Gradle Pitest Plugin line to your %s file:</p>
             <em>%s</em>
+            <p>See more here: %s</p><!--anchors done seem to work in the tool window we have created-->
             <p>And see the pitest docs for missing configurations of pitest</p>
         """.trimIndent()
         private val ERROR_MESSAGE_COMPANION_PLUGIN_MISSING = """<b>The Override Companion Plugin is missing.</b>
             <p>Please add the following line to your %s file:</p>
             <em>%s</em>
         """.trimIndent()
-
-        enum class BuildFile {
-            KOTLIN, GROOVY, NONE
-        }
+        private const val TASK_NAME = "pitmutationmateStatusCheck"
     }
 }
